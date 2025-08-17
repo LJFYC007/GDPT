991b27378a21becc36e19cb0cb5da9604dd33d05
Lu Jinfan <ljfcnyali@gmail.com>
2025-08-17 12:09:59 +0800

    WIP reflecting

--- scripts/GDPT.py
+++ scripts/GDPT.py
@@ -35,13 +35,13 @@ def render_graph_PathTracer():
     # g.markOutput("AccumulatePassX.output")
     # g.markOutput("AccumulatePassY.output")
 
-    ErrorMeasureXPass = createPass("ErrorMeasurePass", {'ReferenceImagePath': 'E:\\GDPT\\minimal_result\\reference-gradientX.exr', 'UseLoadedReference': True, 'SelectedOutputId': 'Source'})
+    ErrorMeasureXPass = createPass("ErrorMeasurePass", {'ReferenceImagePath': 'E:\\GDPT\\minimal_result_bathroom2\\reference-gradientX.exr', 'UseLoadedReference': True, 'SelectedOutputId': 'Source'})
     g.addPass(ErrorMeasureXPass, "ErrorMeasureXPass")
     g.addEdge("AccumulatePassX.output", "ErrorMeasureXPass.Source")
     # g.markOutput("AccumulatePassX.variance")
     g.markOutput("ErrorMeasureXPass.Output")
 
-    ErrorMeasureYPass = createPass("ErrorMeasurePass", {'ReferenceImagePath': 'E:\\GDPT\\minimal_result\\reference-gradientY.exr', 'UseLoadedReference': True, 'SelectedOutputId': 'Source'})
+    ErrorMeasureYPass = createPass("ErrorMeasurePass", {'ReferenceImagePath': 'E:\\GDPT\\minimal_result_bathroom2\\reference-gradientY.exr', 'UseLoadedReference': True, 'SelectedOutputId': 'Source'})
     g.addPass(ErrorMeasureYPass, "ErrorMeasureYPass")
     g.addEdge("AccumulatePassY.output", "ErrorMeasureYPass.Source")
     # g.markOutput("AccumulatePassY.variance")
